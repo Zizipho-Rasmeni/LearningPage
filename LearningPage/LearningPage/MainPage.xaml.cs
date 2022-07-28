014500b213da269785def45ebd034a2eec5f61a1
Zizipho-Rasmeni <4271169@myuwc.ac.za>
2022-07-28 15:16:31 +0200

    Add project files.

---
+++ LearningPage/LearningPage/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LearningPage
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Blogs_Clicked(object sender, EventArgs e)
+        {
+           // Navigation.PushAsync(new Blogs());
+        }
+
+        private void Videos_Clicked(object sender, EventArgs e)
+        {
+           // Navigation.PushAsync(new Video());
+        }
+    }
+}
---
+++ LearningPage/LearningPage/Video.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LearningPage
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Video : ContentPage
+    {
+        public Video()
+        {
+            InitializeComponent();
+        }
+
+        private void Videos_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Video());
+        }
+
+
+    }
+}
